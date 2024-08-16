b9a6184b84ee7492706d8a93bff64a55bd019e83
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:26 -0700

    exported from Skill Struck

---
+++ python1 /string-method-cont.py
@@ -0,0 +1,34 @@
+#Checkpoint
+string1 = "I love my mom and dad!"
+print(string1[0:7])
+print(string1.split())
+print(len(string1))
+print(string1.find("I"))
+
+#Challenge - How many Words?
+enter = str(input("Enter a sentence."))
+print(len(enter.split()))
+
+#Challenge - First and Last
+#This challenge was uncompleted.
+
+#Challenge - String Characcter Selection
+#This challenge was uncompleted.
+
+#Challenge - Second Time
+#This challenge was uncompleted.
+
+#Challenge - Cut the String
+enter = str(input("Enter a word with even amount of letters."))
+n = len(enter)
+mid = n//2
+first_half = enter[:mid]
+second_half = enter[mid:]
+end = second_half + first_half
+print(end)
+
+#Challenge - Remove String Section
+#This challenge was uncompleted.
+
+#Challenge - Reverse String Section
+#This challenge was uncompleted.
