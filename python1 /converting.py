50823b5b02d0636a00c50a73e067f94cec1ba40e
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:21 -0700

    exported from Skill Struck

---
+++ python1 /converting.py
@@ -0,0 +1,25 @@
+#Checkpoint
+melons = "4"
+print(melons)
+oranges = 3.5
+float_1 = int(oranges)
+print(float_1)
+peaches = 5
+float_2 = float(peaches)
+print(float_2)
+
+#Challenge - 
+age = int(input("Please input your age."))
+age_18 = (age + 18)
+print(age_18)
+
+#Challenge - 
+decimal = float(input("Please enter a decimal number."))
+decimal_2 = int(decimal)
+print(decimal_2)
+
+#Challenge - 
+name = input("What is your name?")
+age = int(input("What is your age?"))
+mult = (name * age)
+print(mult)
