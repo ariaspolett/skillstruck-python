edb850fe288b33e225819dab53a2570bfc82bad3
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:28 -0700

    exported from Skill Struck

---
+++ python1 /python-else-if-statements.py
@@ -0,0 +1,50 @@
+#Checkpoint
+coins = 10
+if coins > 20:
+    print("You have more than enough money to buy a puppy")
+elif coins == 20: 
+    print("You have exactly enough to buy a puppy")
+else: 
+    print("You do not have enough to buy a puppy")
+
+#Challenge - Greater than?
+number = input("enter a number")
+number2 = input("enter another number")
+if number > number2: 
+    print("The first number is larger.")
+elif number2 > number:
+    print("The second number is larger.")
+elif number == number2:
+    print("The numbers are the same.")
+
+#Challenge - Smallest of Three Numbers
+#This challenge was uncompleted.
+
+#Challenge - Equal Numbers
+#This challenge was uncompleted.
+
+#Challenge - Forest Adventure
+choice = input("which direction should you go?")
+if choice == "left":
+    print("You walk down the left path and your eyes widen when you realize the building is an abandoned Taco-Bell. You walk inside and explore. Unfortunately, you do not make it through the night. The rats got you.")
+elif choice == "right":
+    print("You walk down the right path and you frown softly as you progress further and the last light of the sun fades through the tick bushes. You continue walking until you come across a well. You approach the well and take a look inside. You jolt back when you gaze upon the face of a young girl's lifeless eyes.")
+elif choice == "center": 
+    print("You walk down the center path and you feel yourself tense up as the rustling sounds grow louder. You feel the hairs on your neck stand up and you freeze as you gaze upon a creek filled with bones.")
+else: 
+    print("You decided to turn around and go home. You should be glad. You could've met a terrible end.")
+
+#Challenge - Order of Outlier
+#This challenge was uncompleted.
+
+#Challenge - Order the Digits
+#This challenge was uncompleted.
+
+#Challenge - Days in a Month
+#This challenge was uncompleted.
+
+#Challenge - Same Color
+#This challenge was uncompleted.
+
+#Challenge - Leap Year
+#This challenge was uncompleted.
