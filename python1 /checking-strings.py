2e48b3cdbf6215dc38f9d0a7736daae2f495f7dc
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:24 -0700

    exported from Skill Struck

---
+++ python1 /checking-strings.py
@@ -0,0 +1,24 @@
+#Checkpoint
+string1 = "I love ice cream!"
+check = "ice" not in string1
+print(check)
+
+#Challenge - Check for Word
+string1 = input("please enter a sentence")
+check = "armadillo" in string1
+print(check)
+
+#Challenge - Check for Letter
+string1 = input("please enter a sentence")
+check = "a" in string1
+print(check)
+
+#Challenge - Check for Number
+string1 = input("please enter a sentence")
+check = "1" in string1
+print(check)
+
+#Challenge - Are Cassowaries Dangerous?
+passage = "Cassowaries, genus Casuarius, are ratites (flightless birds without a keel on their sternum bone) that are native to the tropical forests of New Guinea (Papua New Guinea and Indonesia), East Nusa Tenggara, the Maluku Islands, and northeastern Australia. There are three extant species. The most common of these, the southern cassowary, is the third-tallest and second-heaviest living bird, smaller only than the ostrich and emu. Cassowaries feed mainly on fruit, although all species are truly omnivorous and will take a range of other plant food, including shoots and grass seeds, in addition to fungi, invertebrates, and small vertebrates. Cassowaries are very wary of humans, but if provoked they are capable of inflicting serious, even fatal, injuries to both dogs and people. The cassowary has often been labeled the world's most dangerous bird. Typically, all cassowaries are shy birds that are found in the deep forest. They are adept at disappearing long before a human knows they were there."
+check = "dangerous" in passage 
+print(check)
