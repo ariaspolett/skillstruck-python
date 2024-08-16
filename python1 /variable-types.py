30e4e4342eb39bb2fe6926dc64fdcfc3ddb38312
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:13 -0700

    exported from Skill Struck

---
+++ python1 /variable-types.py
@@ -0,0 +1,43 @@
+#Checkpoint
+peace = "Peace is awesome"
+print(peace)
+love = 12435
+print(love)
+trees = False
+print(trees)
+
+#Challenge - True or False?
+love = False
+hate = True
+
+#Challenge - Print 3 Types
+number = 42
+print(number)
+intro = "Hello!"
+print(intro)
+hate = True
+print(hate)
+
+#Challenge - What's Your Age?
+age = input("What is your age?")
+print("You are " + age + " years old.")
+
+#Challenge - My Weekend
+did_do = "I went to a quince."
+wish_you_did = "I wish had been able to spend more time coding."
+meals = 7
+homework = 2
+play_games = True
+read = False
+
+#Challenge - About Me Quiz
+is_scared_of_heights = True
+print(is_scared_of_heights)
+loves_dogs = True
+print(loves_dogs)
+has_brown_eyes = True
+print(has_brown_eyes)
+knows_html = False
+print(knows_html)
+ate_breakfast_today = True
+print(ate_breakfast_today)
