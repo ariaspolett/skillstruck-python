8520f35327bdf5f53e57f3fef28bd50b2b914bf5
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:16 -0700

    exported from Skill Struck

---
+++ python1 /input-output.py
@@ -0,0 +1,41 @@
+#Checkpoint
+color = input('What_is_your_favorite_color?')
+print("Your favorite color is " + color)
+
+#Challenge - Print the Dessert
+favorite_dessert = input('What is your favorite dessert?')
+print(favorite_dessert)
+
+#Challenge - Print A Longer Sentence
+name = input('Please enter your name.')
+print('Welcome to the website '+ name)
+
+#Challenge - Hello, Harry!
+first_name = input('What is your first name?')
+print('Hello, '+ first_name + '!')
+
+#Challenge - Ice Cream Order
+ice_cream_flavor = input('What ice cream flavor do you want?')
+print(ice_cream_flavor)
+mix_in_order = input('What mix-in do you want?')
+print(mix_in_order)
+topping_order = input('What topping do you want?')
+print(topping_order)
+
+#Challenge - Compliments
+name = input('What is your name?')
+compliment = input('What is your favorite compliment?')
+print('Hello '+ name + ' I would like to give you a compliment. ' + compliment)
+
+#Challenge - Guitar Class
+name = input("What is your name?")
+experience = input("What kind of music experience do you have?")
+print("Hello " + name + " welcome to the guitar class! Your music experience: " + experience)
+
+#Challenge - About Me!
+question_1 = input('What do you like to do?')
+question_2 = input('What is your favorite movie?')
+question_3 = input('When is the BEST time of day?')
+question_4 = input('What pet peeves do you have?')
+question_5 = input('What is your favorite color?')
+print(question_5)
