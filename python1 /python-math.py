fd9290c4b348f5102a2b0a07cfd7aa5a2bdca974
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:22 -0700

    exported from Skill Struck

---
+++ python1 /python-math.py
@@ -0,0 +1,38 @@
+#Checkpoint
+caterpillars = 3
+leaves = 25
+print(caterpillars * leaves)
+
+#Challenge - Adding Numbers
+number1 = int(input("What is your first number?"))
+number2 = int(input("What is your second number?"))
+sum = number1 + number2
+print(sum)
+
+#Challenge - Number Neighbors
+number = int(input("enter a number."))
+one_less = str(number - 1)
+one_more = str(number + 1)
+comma = (",")
+number_neighbors = (one_less + comma + one_more)
+print(number_neighbors)
+
+#Challenge - Group Gift
+number_people = int(input("How many people will help?"))
+number_money = int(input("How much money will everyone contribute?"))
+total = number_people * number_money
+print(total)
+
+#Challenge - Inheritance
+family = int(input("How many family members will split the inheritance?"))
+total = (48682.76/family)
+print("If " + str(family) + " people split the inheritance, each person would get " + str(total) + " dollars.")
+
+#Challenge - What's the Century?
+year = int(input("What year is it?"))
+century = int(year/100)
+century2 = (century + 1)
+print(century2)
+
+#Challenge - Debugging Challenge 2
+#This challenge was uncompleted.
