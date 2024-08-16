c9b06a37794fa36318b47189b863dfecc7ca0f24
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:23 -0700

    exported from Skill Struck

---
+++ python1 /modulus.py
@@ -0,0 +1,27 @@
+#Checkpoint
+number = int(input("enter a number: "))
+print(number % 7)
+
+#Challenge - Orange Sharing
+family = int(input("Enter number of family members: "))
+oranges = int(input("Emter number of oranges: "))
+print(oranges % family)
+
+#Challenge - Print the last two numbers
+number = int(input("Enter a number larger than 10: "))
+print(number % 100)
+
+#Challenge - Separate the numbers
+number = int(input("Enter a number with two digits: "))
+tens = int(number/10)
+ones = int(number%10)
+print(str(tens) + " " + str(ones))
+
+#Challenge - Reverse the number
+number = int(input("Enter a number with two digits: "))
+tens = int(number/10)
+ones = int(number%10)
+print(str(ones) + str(tens))
+
+#Challenge - Find the Ten's Place
+#This challenge was uncompleted.
