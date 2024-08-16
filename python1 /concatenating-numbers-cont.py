7347d67878a93f3c606018d202e67281e1a938b4
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:25 -0700

    exported from Skill Struck

---
+++ python1 /concatenating-numbers-cont.py
@@ -0,0 +1,29 @@
+#Checkpoint
+string1 = "I have {} fish. I want {} chocolates!"
+thing = 12
+number = 32
+print(string1.format(thing, number))
+
+#Challenge - Sum the Digits
+three = input("enter a three-digit number")
+first = int(three[0])
+second = int(three[1])
+third = int(three[2])
+sum1 = int(first + second + third)
+print("The sum of those digits is " + str(sum1))
+
+#Challenge - Trip Planner
+one_day = int(input("Number of miles your car can drive in a day."))
+total = int(input("Total number of miles driven to get to destination."))
+days = float(total / one_day)
+road_trip = "The total number of days you will need to drive there is {}"
+print(road_trip.format(days))
+
+#Challenge - First Decimal 
+#This challenge was uncompleted.
+
+#Challenge - Cookie Cost Calculator
+#This challenge was uncompleted.
+
+#Challenge - What day is it?
+#This challenge was uncompleted.
