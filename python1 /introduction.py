7840514a768b5978b1d4eac7fce622f22ca91a61
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:15 -0700

    exported from Skill Struck

---
+++ python1 /introduction.py
@@ -0,0 +1,25 @@
+#Checkpoint
+print("Hello World")
+
+#Challenge - First Message
+print("I love to code!")
+
+#Challenge - Favorite Colors
+print("Blue")
+print("Red")
+print("Black")
+
+#Challenge - What's Your Order?
+print("Pizza")
+print("Fries")
+print("Root Beer")
+
+#Challenge - Happiest Moment
+print("I don't really have a specific memory that I can call my happiest moment. All I know is that I'm happiest when I'm with my family or alone. Spending time with others is something I enjoy when I have the patience for it. I'm introverted in nature so I need time to recharge otherwise I'm quite irritable. I enjoy most activities so I have many happy memories.")
+
+#Challenge - Your Favorite Things to Do
+print("I want to travel")
+print("I want to get rich.")
+print("I want to go to Mexico.")
+print("I want to become a software developer.")
+print("I want to live in a penthouse.")
