e03662a8946da813c2e3d4ed75ef3530bfcadf4b
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:26 -0700

    exported from Skill Struck

---
+++ python1 /lists.py
@@ -0,0 +1,25 @@
+#Checkpoint
+candy = ["Milky Ways" , "Twix" , "Hersheys" , "Kisses"]
+pieces = [1000, 2000 , 3000, 500]
+print(candy)
+print(pieces)
+
+#Challenge - List of Friends
+friends = [Ayanna, Nicole, North]
+print(friends)
+
+#Challenge - Reason
+reasons = ["Helps you understand technology" , "Expands knowledge" , "Creates a deeper level of persistence"]
+print(reasons)
+
+#Challenge - Family's Age
+ages = [42, 42, 19, 16, 14, 12]
+print(ages)
+
+#Challenge - Places You've Visited
+places = ["Disneyland" , "Parthenon" , "Delphi" , "Athens" , "Ocean"]
+print(places)
+
+#Challenge - Remember
+memories = [56, 3, "Ayanna", "COVID", 9]
+print(memories)
