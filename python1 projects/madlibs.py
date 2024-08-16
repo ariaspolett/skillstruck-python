05fc4732c574355f6c0af56a904024c71a5a7ec4
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:17 -0700

    exported from Skill Struck

---
+++ python1 projects/madlibs.py
@@ -0,0 +1,7 @@
+noun1 = input("Choose a noun")
+pnoun1 = input("Choose a plural noun")
+noun2 = input("Choose another noun")
+place = input("Choose a place")
+adjective = input("Choose an adjective")
+
+print("Did you know that Batman has a " + noun1 + "?" + " Batman likes to " + pnoun1 + " as much as he loves to fight crime. One day, Batman will have a " + noun2 + " and will learn the true meaning of happiness. Not only will he finally feel " + adjective + ", he'll begin to enjoy visiting " + place + ".")
