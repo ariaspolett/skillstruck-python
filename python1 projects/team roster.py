55ade6f1da6734dac7d50dcf708f34d703d81644
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:19 -0700

    exported from Skill Struck

---
+++ python1 projects/team roster.py
@@ -0,0 +1,13 @@
+print("Layla " + str(12))
+print("Kaveh " + str(5))
+print("Chongyun " + str(1))
+print("Xingqiu " + str(2))
+print("Razor " + str(4))
+print("Bennett " + str(3))
+print("Zhongli " + str(13))
+print("Tartaglia " + str(11))
+print("Ayato " + str(6))
+print("Ei " + str(16))
+string1 = "Welcome to the team! Your team number is {}"
+number = 1
+print(string1.format(number))
