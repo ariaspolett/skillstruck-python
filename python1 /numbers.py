e85204b6113bf2a9409d714024da6e5096f1ea79
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:13 -0700

    exported from Skill Struck

---
+++ python1 /numbers.py
@@ -0,0 +1,43 @@
+#Checkpoint
+dogs = 12
+bags_of_dog_food = 6.5
+
+#Challenge - Feet in a Meter
+feet_in_meters = 3.28084
+print(feet_in_meters)
+
+#Challenge - Sale Trick
+hamburger = 5
+hamburger_2 = 4.99
+candy = 3
+candy_2 = 2.99
+movie_ticket = 9
+movie_ticket_2 = 8.99
+video_game = 14
+video_game_2 = 13.99
+
+#Challenge - Outdoors and Indoors
+rank_outdoors = 4
+print(rank_outdoors)
+rank_indoors = 9
+print(rank_indoors)
+time_outdoors = 20.0
+print(time_outdoors)
+time_indoors = 80.0
+print(time_indoors)
+
+#Challenge - Specific Answers
+Teams = 3
+Teams_2 = 2.5
+New_things = 3
+New_things_2 = 3.5
+outside = 2
+outside_2 = 2.3
+travel = 5
+travel_2 = 5.0
+school = 1
+school_2 = 1.5
+
+#Challenge - How many pets?
+average_pets = 6.5
+print(average_pets)
