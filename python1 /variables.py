72284ee3eb309b19e4851a89d2bd0efe6024048d
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:16 -0700

    exported from Skill Struck

---
+++ python1 /variables.py
@@ -0,0 +1,41 @@
+#Checkpoint
+food = "pizza"
+food = "burgers"
+food = "fries"
+print(food)
+
+#Challenge - Wood Chuck
+how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood = "tongue twisters"
+print(how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood)
+
+#Challenge - Restaurant Favorites
+main_course = "pasta"
+print(main_course)
+side = "bread sticks"
+print(side)
+drink = "root beer"
+print(drink)
+
+#Challenge - How many in a beetle?
+How_many_hambugers_could_you_fit_inside = 365
+print(How_many_hambugers_could_you_fit_inside)
+How_many_people_could_you_fit_inside = 6
+print(How_many_people_could_you_fit_inside)
+How_many_basketballs_could_you_fit_inside = 30
+print(How_many_basketballs_could_you_fit_inside)
+
+#Challenge - Who is your hero?
+hero1 = "percy jackson"
+print(hero1)
+hero2 = "my mom"
+print(hero2)
+hero3 = "my dad"
+print(hero3)
+
+#Challenge - Riddle Variables
+what_goes_up_when_rain_comes_down = "umbrella"
+print(what_goes_up_when_rain_comes_down)
+what_is_full_of_holes_but_holds_water = "sponge"
+print(what_is_full_of_holes_but_holds_water)
+what_can_you_not_keep_until_you_give_it = "your_word"
+print(what_can_you_not_keep_until_you_give_it)
