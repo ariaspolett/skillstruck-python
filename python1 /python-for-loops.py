a7cad7f4e90b0f8cb1cd8f4eaa31d5dbd5d7a125
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:28 -0700

    exported from Skill Struck

---
+++ python1 /python-for-loops.py
@@ -0,0 +1,21 @@
+#Checkpoint
+animals = ["hedgehog", "sea urchin", "echidna", "porcupine"]
+for x in animals:
+    print(x + " is the spikiest animal ever!")
+
+#Challenge - Automatic Numbers
+var1 = int(input('Enter a number'))
+var2 = int(input('Enter another number.'))
+for x in range(var1, var2 + 1):
+    print(x)
+
+#Challenge - Add the Numbers
+num1 = int(input("enter a number"))
+num2 = int(input("enter a number"))
+var = 0
+for x in range (num1, num2):
+    var = var + x
+print(var)
+
+#Challenge - Factorial
+#This challenge was uncompleted.
