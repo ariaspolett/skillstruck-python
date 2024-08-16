5912ad5d2e57670ca65aebb95fe9f516c74dbdeb
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:23 -0700

    exported from Skill Struck

---
+++ python1 /strings.py
@@ -0,0 +1,45 @@
+#Checkpoint
+multiline = """This is a multiline string. I hope 
+I'm doing this correctly. I want to do it right. I have no idea what I'm writing right now."""
+print(multiline) 
+
+#Challenge - Favorite Quotes
+short_quote = "The only thing we have to fear is fear itself."
+print(short_quote)
+long_quote = """Remember, democracy never lasts long.
+ It soon wastes, exhausts, and murders itself. There never was a democracy yet that did not commit suicide"""
+print(long_quote)
+
+#Challenge - Journal Entry
+journal = """Today I woke up at 8 AM. I took a shower 
+and quickly got changed. Then I went to my high school for a tour since I just transferred there. I met two new people and one old friend. It was nice because we just talked for a long time and ate lunch. Then it was time for me to leave for EVIT. I walked to the front office and exited through there. I put on my EVIT shirt and waited 7 minutes for my bus to arrive. It took about 20 minutes to get to EVIT. When I arrived at EVIT, I sat down to take my spiral test, it was difficult. I got an 80% on my first try which was the goal. I want to try again tomorrow for a better score though"""
+print(journal)
+
+#Challenge - Project Ideas
+ideas = """I don't really have a clear goal in mind
+but I would love to help others' ideas come to life."""
+print(ideas)
+
+#Challenge - Best Names
+name1 = "Aurelius"
+print(name1)
+name2 = "Peresphone"
+print(name2)
+name3 = "Thalia"
+print(name3)
+name4 = "Yikoko"
+print(name4)
+
+#Challenge - Garden and Orchard
+fruit1 = "strawberries"
+print(fruit1)
+fruit2 = "blueeberries"
+print(fruit2)
+fruit3 = "mangos"
+print(fruit3)
+veggie1 = "cucumbers"
+print(veggie1)
+veggie2 = "potatoes"
+print(veggie2)
+veggie3 = "carrots"
+print(veggie3)
