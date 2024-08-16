d74dd55b8192440b99e4dd8e228b31e76ee9ee3e
ariaspolett <ariaspolett21@gmail.com>
2024-08-16 14:04:12 -0700

    exported from Skill Struck

---
+++ python1 /syntax-comments.py
@@ -0,0 +1,40 @@
+#Checkpoint 
+paper = "What is paper used for?"
+print(paper)
+#paper is also hurtful. papercuts are the worst.
+
+#Challenge - Commenting Your Parents -----------------
+print("Necesitas ser fuerte y no llorar")
+# They want me to be more strong-willed.
+print("Tienes que estudiar para tener una vida mejor")
+# They want me to focus more on school to have a good life in the future.
+print("Ten modales y se amable")
+# Being kind doesn't cost anything.
+
+#Challenge - How are you feeling today? --------------
+#I am feeling calm and classical music envokes a calm feeling in me.
+sound = "The sound of a classical piano"
+#I associate blue with various emotions but right now, I think it means tranquility.
+color = "I am feeling like the color blue"
+#Quesadillas are a comfort food of mine so it's calming
+flavor = "I am feeling like the flavor of quesadillas"
+
+#Challenge - Snow Day! -------------------------------
+#You have to be careful when coding the variables because they don't really do much in this challenge
+morning_q = input('What do you do in the morning?')
+morning = "morning_q"
+afternoon_q = input('What do you do in the afternoon?')
+afternoon = "afternoon_q"
+evening_q = input('What do you do in the evening?')
+evening = "evening_q"
+print(morning_q)
+print(afternoon_q)
+print(evening_q)
+
+#Challenge - Your Favorite Movies -------------------- 
+print("My first favorite movie is Harry Potter and Sorcerer's Stone.")
+#I really like Harry Potter and my favorite of the series is the first one.
+print("My second favorite movie is the Quiet Place 1.")
+#I like suspense movies with a good amount of horror and thriller.
+print("My third favorite movie is Venom.")
+#I love the humor and plot.
