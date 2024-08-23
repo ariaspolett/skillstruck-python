e1c462ef993e9eb028299fbd0d5dc0a199d0bbf3
ariaspolett <ariaspolett21@gmail.com>
2024-08-23 12:24:09 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,6 +1,1 @@
-number = int(input('enter a number'))
-total = 1
-for x in range(1, number + 1):
-    total = x * number
-    total = x * number
-    print(total)
+month = input("enter a number 1 through 12")
