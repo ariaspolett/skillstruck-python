a232d243974d3de4bdd247d889c8194da25ca618
ariaspolett <ariaspolett21@gmail.com>
2024-08-27 13:46:53 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,1 +1,20 @@
-month = input("enter a number 1 through 12")
+options = [.20, .30, .40, .50, .60, .70]
+
+choice = int(input("Which number will you pick? 0-5")) 
+
+scratch_off = options[choice]
+print(scratch_off)
+
+price = 29.95
+
+discount = price * scratch_off
+print(discount)
+
+total = price - discount
+print(total)
+
+people = int(input("How many people?"))
+
+per_person =  total/people
+
+print(round(per_person,2))
