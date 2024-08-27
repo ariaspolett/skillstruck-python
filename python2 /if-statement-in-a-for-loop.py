bd2e4e6d4c84e7f724a32c9e11e68ddbb205d73b
ariaspolett <ariaspolett21@gmail.com>
2024-08-27 13:47:00 -0700

    exported from Skill Struck

--- python2 /if-statement-in-a-for-loop.py
+++ python2 /if-statement-in-a-for-loop.py
@@ -1,10 +1,54 @@
-snickers = int(input("How many snickers did you get?"))
-nerds = int(input("How many nerds did you get?"))
-butterfingers = int(input("How many butterfingers did you get?"))
-total = snickers + nerds + butterfingers
-again = str(total)
-snicker = str(snickers)
-nerd = str(nerds)
-butterfinger = str(butterfingers)
-candy_count = ("This year, you got " + snicker + " snickers, " + nerd + " nerds, and " + butterfinger + " butterfingers. The total number of these candies is " + str(total) + " candies.")
-print(candy_count)
+#Checkpoint - 
+foods = ["Radish", "Salmon", "Shrimp", "Ceviche", "Beets", "Asparagus"]
+for x in foods:
+	if foods == "mushroom":
+		print(x + " is gross")
+	elif foods == "broccoli":
+		print(x + " is gross")
+	elif foods == "fish":
+		print(x + " is gross")
+	else:
+		print(x + " is gross")
+
+#Challenge - Count the Zeros
+my_list = [int(n) for n in input("Input a list of numbers").split()]
+total = 0
+for number in my_list :
+	if number == 0:
+		total += 1
+print(total)
+
+#Challenge - Find the Even Numbers
+my_list = [int(n) for n in input().split()]
+for number in my_list:
+    if number % 2 == 0:
+        print(number)
+
+#Challenge - World Travel
+my_list = input().split()
+countries = my_list
+for country in countries:
+    if "p" in country.lower() or "P" in country.upper():
+        print(country)
+
+#Challenge - Better than Average
+my_list = [int(n) for n in input().split()]
+total = 0
+for x in my_list:
+	total = total + x
+
+average = total/len(my_list)
+
+for x in my_list :
+	if x > average:
+		print(x)
+
+#Challenge - Color Inspector
+choice = input("Choose a letter")
+letter = choice
+my_list = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white", "gray", "pink", "indigo", "brown", "tan", "gold", "silver"]
+total = 0
+for x in my_list:
+	if letter in x:
+		total += 1
+print("There are " + str(total) + " items in the list that have the letter " + choice + " in it.")
