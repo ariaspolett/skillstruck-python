7c5b25da1d2226e27302384cb4348cd947ec05b3
ariaspolett <ariaspolett21@gmail.com>
2024-08-28 13:57:18 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,6 +1,11 @@
-string = input("enter a sentence with at least two j's")
-firstj = int(string.find("j"))
-secondj = int(string.rfind("j"))
-js = string[firstj:secondj +1]
-reverse = js[len(js)::-1]
-print(reverse)
+number = float(input("enter a number w/ a decimal"))
+mod = number%100
+mods = mod%10
+modss = mods%1
+float_to_str = str(modss)
+decimal_find = float_to_str.find(".")
+length_of_decimal = len(float_to_str)
+cut = float_to_str[decimal_find+1:3]
+cuts = int(cut)
+fin = "The first decimal digit is {}"
+print(fin.format(cuts))
