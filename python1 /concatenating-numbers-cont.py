c8d3d4cdfa094d6e09df3335514abf4279492530
ariaspolett <ariaspolett21@gmail.com>
2024-08-28 14:22:39 -0700

    exported from Skill Struck

--- python1 /concatenating-numbers-cont.py
+++ python1 /concatenating-numbers-cont.py
@@ -33,7 +33,15 @@ fin = "The first decimal digit is {}"
 print(fin.format(cuts))
 
 #Challenge - Cookie Cost Calculator
-#This challenge was uncompleted.
+dollars = int(input("how many dollars does 1 cookie cost."))
+cents = int(input("how many cents does 1 cookie cost."))
+cookie = int(input("how many cookies do you want to buy?"))
+dollars_to_cents = dollars*100
+total_cost = dollars_to_cents + cents
+total_money = total_cost/100
+total = total_money * cookie
+message = "The total cost of {} cookies is ${}"
+print(message.format(cookie, total))
 
 #Challenge - What day is it?
 #This challenge was uncompleted.
