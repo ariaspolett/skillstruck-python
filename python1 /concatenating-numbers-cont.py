99d7eb64c636fe2f3d983f10a6b24f62b7fd2f53
ariaspolett <ariaspolett21@gmail.com>
2024-08-28 13:57:22 -0700

    exported from Skill Struck

--- python1 /concatenating-numbers-cont.py
+++ python1 /concatenating-numbers-cont.py
@@ -20,7 +20,17 @@ road_trip = "The total number of days you will need to drive there is {}"
 print(road_trip.format(days))
 
 #Challenge - First Decimal 
-#This challenge was uncompleted.
+number = float(input("enter a number w/ a decimal"))
+mod = number%100
+mods = mod%10
+modss = mods%1
+float_to_str = str(modss)
+decimal_find = float_to_str.find(".")
+length_of_decimal = len(float_to_str)
+cut = float_to_str[decimal_find+1:3]
+cuts = int(cut)
+fin = "The first decimal digit is {}"
+print(fin.format(cuts))
 
 #Challenge - Cookie Cost Calculator
 #This challenge was uncompleted.
