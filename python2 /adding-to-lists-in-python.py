c50169052073b635dca09634e565266747e1f3db
ariaspolett <ariaspolett21@gmail.com>
2024-08-29 14:19:45 -0700

    exported from Skill Struck

--- python2 /adding-to-lists-in-python.py
+++ python2 /adding-to-lists-in-python.py
@@ -48,3 +48,10 @@ else:
     print(family)
 
 #Challenge - Donation
+my_list = [int(n) for n in input("Input a list of numbers").split()] 
+cash_back = []
+for x in my_list:
+	if x >= 5:
+		cash_back_amount = x * .1
+		cash_back.append(round(cash_back_amount,1))
+print(cash_back)
