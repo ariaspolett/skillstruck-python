5c0b86d37956e60e82057d3b43c06b9364b58f9a
ariaspolett <ariaspolett21@gmail.com>
2024-08-29 14:32:34 -0700

    exported from Skill Struck

--- python1 /python-else-if-statements.py
+++ python1 /python-else-if-statements.py
@@ -97,7 +97,31 @@ else:
     print("nothing")
 
 #Challenge - Days in a Month
-#This challenge was uncompleted.
+number = int(input("enter a number 1-12"))
+if number == 1:
+    print("31")
+elif number == 2:
+    print("28")
+elif number == 3:
+    print("31")
+elif number == 4:
+    print("30")
+elif number == 5:
+    print("31")
+elif number == 6:
+    print("30")
+elif number == 7:
+    print("31")
+elif number == 8:
+    print("31")
+elif number == 9:
+    print("30")
+elif number == 10:
+    print("31")
+elif number == 11:
+    print("30")
+elif number == 12:
+    print("31").
 
 #Challenge - Same Color
 #This challenge was uncompleted.
