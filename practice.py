49de86531265ddbda481bed027e5159b60a9e40b
ariaspolett <ariaspolett21@gmail.com>
2024-08-30 13:22:34 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,1 +1,20 @@
-my_list = [int(n) for n in input().split()]
+num1 = int(input("enter number 1-8"))
+num2 = int(input("enter number 1-8"))
+num3 = int(input("enter number 1-8"))
+num4 = int(input("enter number 1-8"))
+square1 = num1 + num1
+square2 = num3 + num4
+if square1%2 == 0 and square2%2 == 0:
+    print("Yes")
+else: 
+    print("No")
+    num1 = int(input("enter number 1-8"))
+num2 = int(input("enter number 1-8"))
+num3 = int(input("enter number 1-8"))
+num4 = int(input("enter number 1-8"))
+square1 = num1 + num1
+square2 = num3 + num4
+if square1%2 == 0 and square2%2 == 0:
+    print("Yes")
+else: 
+    print("No")
