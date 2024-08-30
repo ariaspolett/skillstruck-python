5c02c5d218eb0bb57a4f196fba665431e73a03df
ariaspolett <ariaspolett21@gmail.com>
2024-08-30 13:22:40 -0700

    exported from Skill Struck

--- python1 /python-else-if-statements.py
+++ python1 /python-else-if-statements.py
@@ -124,7 +124,16 @@ elif number == 12:
     print("31").
 
 #Challenge - Same Color
-#This challenge was uncompleted.
+num1 = int(input("enter number 1-8"))
+num2 = int(input("enter number 1-8"))
+num3 = int(input("enter number 1-8"))
+num4 = int(input("enter number 1-8"))
+square1 = num1 + num1
+square2 = num3 + num4
+if square1%2 == 0 and square2%2 == 0:
+    print("Yes")
+else: 
+    print("No")
 
 #Challenge - Leap Year
 #This challenge was uncompleted.
