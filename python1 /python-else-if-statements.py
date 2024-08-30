1796b8edb8623cb4f208393a7b6a3fadfbeb62a4
ariaspolett <ariaspolett21@gmail.com>
2024-08-30 13:32:38 -0700

    exported from Skill Struck

--- python1 /python-else-if-statements.py
+++ python1 /python-else-if-statements.py
@@ -136,4 +136,12 @@ else:
     print("No")
 
 #Challenge - Leap Year
-#This challenge was uncompleted.
+year = int(input("enter a year"))
+if year%4 == 0:
+    print("Leap")
+elif year%400 == 0 and year%100 == 0:
+    print("Leap")
+elif year%100 == 0 and year%400 == 1: 
+    print("Common")
+else:
+    print("Common")
