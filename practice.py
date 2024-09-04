01fc0c4790e076c45b4c6d2015d538b8f25994a3
ariaspolett <ariaspolett21@gmail.com>
2024-09-04 13:39:44 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,22 +1,7 @@
-x = 0
-y = 0
-run=True
-while x<=5 and y<=5 and x>=0 and y>=0 and run == True:
-	direction=input('What way would you like to go (n,s,e,w)? ')
-	if direction == "n":
-		y+=1
-	elif direction == "s":
-		y-=1
-	elif direction == "e":
-		x+=1
-	elif direction == "w":
-		x-=1
-	elif direction=='exit':
-		print('Program exited. You left the grid at (' + str(x) + ',' + str(y) + ')')
-		run=False
-	else:
-		print('Bad input')
-    if not (0 <= x <= 5 and 0 <= y <= 5): 
-        print(f'You left the grid at ({x}, {y})') 
-        run = False 
-print('You left the grid at ' + '(' + str(x) + "," + str(y) + ')')
+length = 4
+def surface_area():
+    print(length*length*6)
+surface_area()
+def volume():
+    print(length*length*length)
+volume()
