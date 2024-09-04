cfec089531c4bc07ea42c8df663d849d11eda704
ariaspolett <ariaspolett21@gmail.com>
2024-09-04 12:07:49 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,6 +1,22 @@
-day_number = int(input("enter a number 1-365"))
-weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-start_of_index = 4
-day_of_week_index = (start_of_index + (day_number - 1))%7
-message = "The day of the week is the number {}"
-print(message.format(day_of_week_index))
+x = 0
+y = 0
+run=True
+while x<=5 and y<=5 and x>=0 and y>=0 and run == True:
+	direction=input('What way would you like to go (n,s,e,w)? ')
+	if direction == "n":
+		y+=1
+	elif direction == "s":
+		y-=1
+	elif direction == "e":
+		x+=1
+	elif direction == "w":
+		x-=1
+	elif direction=='exit':
+		print('Program exited. You left the grid at (' + str(x) + ',' + str(y) + ')')
+		run=False
+	else:
+		print('Bad input')
+    if not (0 <= x <= 5 and 0 <= y <= 5): 
+        print(f'You left the grid at ({x}, {y})') 
+        run = False 
+print('You left the grid at ' + '(' + str(x) + "," + str(y) + ')')
