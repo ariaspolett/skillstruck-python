d552fbb208048df5d381e5c332c032a4ae070b03
ariaspolett <ariaspolett21@gmail.com>
2024-09-06 21:22:02 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -3,21 +3,11 @@
 #trees.pop(remove)
 #print(trees)
 
-#choice = int(input("Enter a number 1-12"))
-#def invitation (choice) :
-	#if choice == 1  or choice == 3 or choice == 5 or choice == 7 or choice == 8 or choice == 10 or choice == 12:
-		#print("31")
-	#elif choice == 2:
-		#print("28")
-	#else:
-		#print("30") 
-#invitation(choice)
-
-def odd_even(num):
-	if num % 2 == 0:
-		print("even")
+choice1 = int(input("What is the first number?"))
+choice2 = int(input("What is the second number?"))
+def my_function(choice1, choice2):
+	if choice1 > choice2:
+		print(choice2)
 	else:
-		print("odd")
-odd_even(43)
-odd_even(67)
-odd_even(44)
+		print(choice1)
+my_function(choice1,choice2)
