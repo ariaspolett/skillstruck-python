19826c77457e2c24be40223a52892b87d767aee7
ariaspolett <ariaspolett21@gmail.com>
2024-09-06 20:56:13 -0700

    exported from Skill Struck

---
+++ python2 /python-function-parameters.py
@@ -0,0 +1,28 @@
+#Checkpoint - 
+def lunch(food):
+    print("I love today's lunch of " + food)
+lunch("pizza")
+lunch("salad")
+lunch("pasta")
+
+#Challenge - Hungry for Apples 2
+fruit = input("Please enter the name of a fruit: ")
+
+def hungryForApples(fruit):
+    print(fruit+fruit+fruit)
+	
+hungryForApples(fruit)
+
+#Challenge - Count the Days in a Month Function
+
+
+#Challenge - Odd/Even Checking Function
+
+
+#Challenge - A Party for the letter C
+
+
+#Challenge - Is it a Leap Year?
+
+
+#Challenge - Area of a Circle
