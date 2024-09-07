1a49bc25450aeb4f38e95f27469afc02fcbba252
ariaspolett <ariaspolett21@gmail.com>
2024-09-06 21:13:34 -0700

    exported from Skill Struck

--- python2 /python-function-parameters.py
+++ python2 /python-function-parameters.py
@@ -7,17 +7,30 @@ lunch("pasta")
 
 #Challenge - Hungry for Apples 2
 fruit = input("Please enter the name of a fruit: ")
-
 def hungryForApples(fruit):
     print(fruit+fruit+fruit)
-	
 hungryForApples(fruit)
 
 #Challenge - Count the Days in a Month Function
-
+choice = int(input("Enter a number 1-12"))
+def invitation (choice) :
+	if choice == 1  or choice == 3 or choice == 5 or choice == 7 or choice == 8 or choice == 10 or choice == 12:
+		print("31")
+	elif choice == 2:
+		print("28")
+	else:
+		print("30") 
+invitation(choice)
 
 #Challenge - Odd/Even Checking Function
-
+def odd_even(num):
+	if num % 2 == 0:
+		print("even")
+	else:
+		print("odd")
+odd_even(43)
+odd_even(67)
+odd_even(44)
 
 #Challenge - A Party for the letter C
 
