507f56e2eb722ff1588db6f8e8607ce8a314ea24
ariaspolett <ariaspolett21@gmail.com>
2024-09-06 21:37:35 -0700

    exported from Skill Struck

---
+++ python2 /python-dictionaries.py
@@ -0,0 +1,16 @@
+#Checkpoint -
+mountains = {"Timpanogos" : 6, "Everest": 10, "Kilimanjaro": 7, "Vesuvius": 5}
+print(mountains)
+print(["Vesuvius"])
+mountains["Kilimanjaro"] = 8
+mountains()
+
+#Challenge - 
+holidays = {"January" : "New Years", "Febuary" : "Valentines", "March" : "love", "May" : "school", "July" : "Fourth", "August" : "birthday", "October" : "halloween", "November" : "grateful", "december" : "gifts", "june" : "mom"}
+print(holidays)
+
+#Challenge - 
+packing = {"shoes": "vans", "socks": "dots", "shirts": "cotton", "pants": "jeans", "pjs": "stripes"}
+packing()
+packing_list = ["shoes", "socks", "shirts", "pants", "pjs"]
+packing_list()
