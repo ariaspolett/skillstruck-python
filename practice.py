fea336037710b3b3656965c87898c19e14440f48
ariaspolett <ariaspolett21@gmail.com>
2024-09-06 21:13:26 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,6 +1,23 @@
-trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
+##trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
+#remove = input("which tree would you like to remvoe?")
+#trees.pop(remove)
+#print(trees)
 
-remove = input("which tree would you like to remvoe?")
+#choice = int(input("Enter a number 1-12"))
+#def invitation (choice) :
+	#if choice == 1  or choice == 3 or choice == 5 or choice == 7 or choice == 8 or choice == 10 or choice == 12:
+		#print("31")
+	#elif choice == 2:
+		#print("28")
+	#else:
+		#print("30") 
+#invitation(choice)
 
-trees.pop(remove)
-print(trees)
+def odd_even(num):
+	if num % 2 == 0:
+		print("even")
+	else:
+		print("odd")
+odd_even(43)
+odd_even(67)
+odd_even(44)
