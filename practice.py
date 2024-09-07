6ea156709f02d42b02c379a7ad864412b789ed53
ariaspolett <ariaspolett21@gmail.com>
2024-09-06 21:41:19 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,7 +1,4 @@
-##trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
-#remove = input("which tree would you like to remvoe?")
-#trees.pop(remove)
-#print(trees)
-
-holidays = {"January" : "New Years", "Febuary" : "Valentines", "March" : "love", "May" : "school", "July" : "Fourth", "August" : "birthday", "October" : "halloween", "November" : "grateful", "december" : "gifts", "june" : "mom"}
-print(holidays)
+trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
+remove = input("which tree would you like to remvoe?")
+trees.pop(remove)
+print(trees)
