608410faa40a95cf32cf77a6198d3e85dfaec340
ariaspolett <ariaspolett21@gmail.com>
2024-09-09 13:17:30 -0700

    exported from Skill Struck

--- python2 /python-dictionaries.py
+++ python2 /python-dictionaries.py
@@ -1,16 +1,25 @@
 #Checkpoint -
 mountains = {"Timpanogos" : 6, "Everest": 10, "Kilimanjaro": 7, "Vesuvius": 5}
 print(mountains)
-print(["Vesuvius"])
+print(mountains["Vesuvius"])
 mountains["Kilimanjaro"] = 8
-mountains()
+print(mountains)
 
-#Challenge - 
+#Challenge - Dictionary Author
 holidays = {"January" : "New Years", "Febuary" : "Valentines", "March" : "love", "May" : "school", "July" : "Fourth", "August" : "birthday", "October" : "halloween", "November" : "grateful", "december" : "gifts", "june" : "mom"}
 print(holidays)
 
-#Challenge - 
-packing = {"shoes": "vans", "socks": "dots", "shirts": "cotton", "pants": "jeans", "pjs": "stripes"}
-packing()
+#Challenge - Dictionaries and Lists
+packing = {"shoes": "vans", "socks": "dots", "shirts": "cotton", "pants": "jeans", "pjs": "stripes", "money" : "greed", "mom" : "warmth", "dad" : "warm", "siblings" : "loud", "family" : "good"}
+print(packing)
 packing_list = ["shoes", "socks", "shirts", "pants", "pjs"]
-packing_list()
+print(packing_list)
+
+#Challenge - Survival
+#This challenge was not completed.
+
+#Challenge - Color Dictionary
+#This challenge was not completed.
+
+#Challenge - All the Pets
+#This challenge was not completed.
