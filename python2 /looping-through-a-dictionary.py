7cfb785bc1d9243f1a66a2f3d934afb9bbaa2f21
ariaspolett <ariaspolett21@gmail.com>
2024-09-09 14:15:57 -0700

    exported from Skill Struck

---
+++ python2 /looping-through-a-dictionary.py
@@ -0,0 +1,28 @@
+#Checkpoint - 
+measurement = {
+    "length" : 4,
+    "width" : 6,
+    "depth" : 7
+}
+for x in measurement.values():
+    print(x)
+
+#Challenge - Secret Beach Day
+amanda_people = int(input("How many people will Amanda bring?"))
+jane_people = int(input("How many people will Jane bring?"))
+group = { "Fred" : 12, "Jackson" : 15, "Sophie" : 20, "Amanda" : amanda_people, "Jane" : jane_people,}
+total = 0
+for x in group.values():
+    total += x
+print(total)
+
+#Challenge - Add it All Together
+
+
+#Challenge - Boxes
+
+
+#Challenge - Shoes
+
+
+#Challenge - Family Banquet
