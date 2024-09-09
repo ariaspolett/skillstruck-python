f5496566a3dcd4799ab973b206739662bacbb84b
ariaspolett <ariaspolett21@gmail.com>
2024-09-09 14:15:48 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,13 +1,5 @@
-work = {
-    "Monday" : 5,
-    "Tuesday" : 8,
-    "Wednesday" : 6,
-    "Thursday" : 7,
-    "Friday" : 5,
-}
-work["Saturday"] = 5
-work.pop(
-    "Wednesday")
-print(len(work))
-if "Friday" in work:
-    print(work)
+radius = int(input("Enter a nnumber for the radius"))
+def area_circle(radius):
+    calculation = 3.14159*radius*radius
+    print(round(calculation, 1))
+area_circle(radius)
