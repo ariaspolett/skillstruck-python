86c55008a339de1103ceda828d7bcdb23cb9fd59
ariaspolett <ariaspolett21@gmail.com>
2024-09-10 13:10:37 -0700

    exported from Skill Struck

--- python2 /multiple-parameters-in-python-functions.py
+++ python2 /multiple-parameters-in-python-functions.py
@@ -24,13 +24,49 @@ def my_function(choice1, choice2):
 my_function(choice1,choice2)
 
 #Challenge - How Mant are Equal Function
-#This challenge was not completed.
+choice1 = int(input("What is the choice1 number?"))
+choice2 = int(input("What is the choice2 number?"))
+choice3 = int(input("What is the choice3 number?"))
+def my_function(choice1, choice2, choice3):
+	if choice1 == choice2 == choice3:
+		print("3")
+	elif choice1 == choice2 or choice2 == choice3 or choice1 == choice3 :
+		print("2")
+	else: 
+		print("0")	
+my_function(choice1, choice2, choice3)
 
 #Challenge - Find the Smallest of Five Function
-#This challenge was not completed.
+num1 = int(input("enter a number"))
+num2 = int(input("enter a number"))
+num3 = int(input("enter a number"))
+num4 = int(input("enter a number"))
+num5 = int(input("enter a number"))
+def my_function(num1, num2, num3, num4, num5):
+	smallest = num1
+	if num2 < num1:
+		smallest = num2
+	if num3 < num2:
+		smallest = num3
+	if num4 < num3:
+		smallest = num4
+	if num5 < num4:
+		smallest = num5
+	print(smallest)
+my_function(num1, num2, num3, num4, num5)
 
 #Challenge -  Spot the Difference Function
-#This challenge was not completed.
+choice1 = int(input("What is the choice1 number?"))
+choice2 = int(input("What is the choice2 number?"))
+choice3 = int(input("What is the choice3 number?")) 
+def my_function(choice1, choice2, choice3):
+	if choice1 == choice3 and choice1 != choice2:
+		print("2")
+	elif choice2 == choice3 and choice1 != choice2:
+		print("1")
+	else: 
+		print("3")
+my_function(choice1, choice2,choice3)
 
 #Challenge - What Day is it Tomorrow Function
 #This challenge was not completed.
