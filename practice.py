d2baa288fd389925f3a3a2a7db890030dfc0358f
ariaspolett <ariaspolett21@gmail.com>
2024-09-10 13:10:29 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,5 +1,11 @@
-radius = int(input("Enter a nnumber for the radius"))
-def area_circle(radius):
-    calculation = 3.14159*radius*radius
-    print(round(calculation, 1))
-area_circle(radius)
+pets = {
+    "fish" : 30,
+    "dogs" : 2,
+    "chickens" : 5,
+    "cats" : 2,
+    "bunnies" : 1
+}
+pets["fish"] = 20
+pets["bunnies"] = 7
+print("Because 10 fish died, and the bunny had 6 babies, you now have " + str(pets["fish"]) + " and " + str(pets["bunnies"]) + " at your house.")
+print(pets)
