859028c084eab5605532a4dd9d14dfd19a760eb3
ariaspolett <ariaspolett21@gmail.com>
2024-09-11 12:44:34 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,11 +1,5 @@
-pets = {
-    "fish" : 30,
-    "dogs" : 2,
-    "chickens" : 5,
-    "cats" : 2,
-    "bunnies" : 1
-}
-pets["fish"] = 20
-pets["bunnies"] = 7
-print("Because 10 fish died, and the bunny had 6 babies, you now have " + str(pets["fish"]) + " and " + str(pets["bunnies"]) + " at your house.")
-print(pets)
+n = int(input("How many keys in dictionary?"))
+dictionary = {}
+for i in range(n):
+    dictionary[i] = i * i
+print(dictionary)
