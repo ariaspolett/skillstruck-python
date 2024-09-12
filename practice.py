7189355b684dc85a451ad2cc401a5ce1eef3b019
ariaspolett <ariaspolett21@gmail.com>
2024-09-12 12:33:28 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,5 +1,2 @@
-my_list = [int(n) for n in input().split()]
-
-for i in range(0, len(my_list) - 1, 2):
-	my_list[i], my_list[i +1] = my_list[i + 1], my_list[i]
-print(my_list)
+reading = open("speech.txt", "r")
+print(reading.read())
