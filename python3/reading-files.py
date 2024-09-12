08cdffc228d7eff21aafffa8b6ace00430a8ee75
ariaspolett <ariaspolett21@gmail.com>
2024-09-12 14:29:33 -0700

    exported from Skill Struck

--- python3/reading-files.py
+++ python3/reading-files.py
@@ -1,4 +1,4 @@
-#LESSON
+  #LESSON
 #hey. this is my first text file. No idea what to write about. I do have a lot of homework today which sucks. Not sure what else I should say.# this is must be within a text file not associated within any folder.
 #Lines 4-6 are what you type in a python file to call forth the text in the txt file
 #writing = open("reading-files.txt", "r")
