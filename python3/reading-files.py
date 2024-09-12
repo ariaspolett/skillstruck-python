6bdeae0b5d397755304e4b71f26038a53d314ed9
ariaspolett <ariaspolett21@gmail.com>
2024-09-12 12:33:38 -0700

    exported from Skill Struck

---
+++ python3/reading-files.py
@@ -0,0 +1,18 @@
+#LESSON
+#hey. this is my first text file. No idea what to write about. I do have a lot of homework today which sucks. Not sure what else I should say.# this is must be within a text file not associated within any folder.
+#Lines 4-6 are what you type in a python file to call forth the text in the txt file
+#writing = open("reading-files.txt", "r")
+#print(writing.read())
+#writing.close()
+
+#Checkpoint - 
+checkpoint = open("reading-files-checkpoint.txt", "r")
+print(checkpoint.read())
+
+#Challenge - Scret Message 
+secret = open("cobra.txt","r")
+print(secret.read())
+
+#Challenge - Read the 
+reading = open("speech.txt", "r")
+print(reading.read())
