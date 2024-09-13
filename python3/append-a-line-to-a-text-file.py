ef211f944fdacdec151cbb7bbb67f083e85e174b
ariaspolett <ariaspolett21@gmail.com>
2024-09-13 12:20:39 -0700

    exported from Skill Struck

---
+++ python3/append-a-line-to-a-text-file.py
@@ -0,0 +1,13 @@
+#Checkpoint - 
+files = open("append-a-line-to-a-text-file.txt", "a")
+files.write("I love programming!")
+files = open("append-a-line-to-a-text-file.txt", "r")
+print(files.read())
+
+#Challenge - Pen Pal
+
+
+#Challenge - Give Credit
+
+
+#Challenge - Custom Line
