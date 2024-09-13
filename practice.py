e2733a5559e52688a3fa367c46d0ea87c9849825
ariaspolett <ariaspolett21@gmail.com>
2024-09-13 12:20:29 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,1 +1,4 @@
-money = open("reading-parts-of-a-file.txt","r")
+files = open("append-a-line-to-a-text-file.txt", "a")
+files.write("I love programming!")
+files = open("append-a-line-to-a-text-file.txt", "r")
+print(files.read())
