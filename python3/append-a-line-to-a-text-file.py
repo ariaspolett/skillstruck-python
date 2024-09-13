2117f2e8a6e4c47b40a276b9c9987dd1f095fcae
ariaspolett <ariaspolett21@gmail.com>
2024-09-13 12:31:58 -0700

    exported from Skill Struck

--- python3/append-a-line-to-a-text-file.py
+++ python3/append-a-line-to-a-text-file.py
@@ -5,9 +5,20 @@ files = open("append-a-line-to-a-text-file.txt", "r")
 print(files.read())
 
 #Challenge - Pen Pal
-
+files = open("letter.txt","a")
+letter.write("From your Pen Pal")
+files = open("letter.txt","r")
+print(files.read())
 
 #Challenge - Give Credit
-
+files = open("report.txt", "a")
+report.write("Quote was said by Gandhi")
+files = open("report.txt", "r")
+print(files.read())
 
 #Challenge - Custom Line
+response = input("Enter a a line you want to add to the text file.")
+files = open("report.txt", "a")
+report.write(response)
+files = open("report.txt", "r")
+print(report.read())
