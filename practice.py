c8219b38f2cf4d38095c232be0933b87303c2b42
ariaspolett <ariaspolett21@gmail.com>
2024-09-17 13:17:39 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,1 +1,9 @@
-nexxt
+cols = [2, 5, 10, 100]
+rows = [int(n) for n in input("Input a list of numbers").split()]
+colls = [ ] 
+for x in rows:
+    col = []
+    for y in cols:
+        col.append(x - y)
+    colls.append(col)
+print(colls)
