2cd34d6a31af70b4c176c961ae62729bcca0a53a
ariaspolett <ariaspolett21@gmail.com>
2024-09-17 14:15:43 -0700

    exported from Skill Struck

---
+++ python2 projects/secret message.py
@@ -0,0 +1,4 @@
+sentence = list(input("Enter a sentence: "))
+
+code = { "a": "q", "b": "x", "c": "j", "d": "z", "e": "v", "f": "p", "g": "k", "h": "u", "i": "w", "j": "y", "k": "b", "l": "t", "m": "r", "n": "d", "o": "h", "p": "s", "q": "e", "r": "a", "s": "i", "t": "f", "u": "l", "v": "g", "w": "o", "x": "n", "y": "c", "z": "m", " ": "1" }
+
