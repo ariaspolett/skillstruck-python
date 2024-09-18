63dac937e14eeff7000ae3438afc872b36d0b605
ariaspolett <ariaspolett21@gmail.com>
2024-09-18 14:26:08 -0700

    exported from Skill Struck

--- python2 projects/secret message.py
+++ python2 projects/secret message.py
@@ -1,4 +1,7 @@
 sentence = list(input("Enter a sentence: "))
-
 code = { "a": "q", "b": "x", "c": "j", "d": "z", "e": "v", "f": "p", "g": "k", "h": "u", "i": "w", "j": "y", "k": "b", "l": "t", "m": "r", "n": "d", "o": "h", "p": "s", "q": "e", "r": "a", "s": "i", "t": "f", "u": "l", "v": "g", "w": "o", "x": "n", "y": "c", "z": "m", " ": "1" }
-
+encoded_sentence = []
+for y in sentence:
+    encoded = code.get(y,y)
+    encoded_sentence.append(encoded)
+print(''.join(encoded_sentence))
