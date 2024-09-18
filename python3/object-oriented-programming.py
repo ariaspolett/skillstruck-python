bdadfc6d42977198cfcd7dd67a2d2ee7169328bb
ariaspolett <ariaspolett21@gmail.com>
2024-09-18 12:27:38 -0700

    exported from Skill Struck

---
+++ python3/object-oriented-programming.py
@@ -0,0 +1,30 @@
+#Checkpoint - 
+class Dog:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.color = color
+        self.size = size
+
+#Challenge - Antique Car Show
+class Car:
+    def __init__(self, kind, color, year):
+        self.kind = kind
+        self.color = color
+        self.year = year
+
+#Challenge - Arborist
+class Tree: 
+    def __init__(self, fruit, height, leaf, flower, seed):
+        self.fruit = fruit
+        self.height = height
+        self.leaf = leaf
+        self.flower = flower
+        self.seed = seed
+
+#Challenge - Bake Sale
+class Dessert:
+    def __init__(self, shape, size, filling, topping):
+        self.shape = shape 
+        self.size = size 
+        self.filling = filling
+        self.topping = topping
