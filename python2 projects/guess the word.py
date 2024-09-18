9c99f7e8a9a277e067f97f706a3b5972657e3002
ariaspolett <ariaspolett21@gmail.com>
2024-09-18 14:04:29 -0700

    exported from Skill Struck

--- python2 projects/guess the word.py
+++ python2 projects/guess the word.py
@@ -1,17 +1,20 @@
 lists = ["money", "family", "hate", "marker", "crayon", "gamble", "creativity", "scrambles", "teacher", "write", "eat"]
 
-number = int(input("enter a number between 0-10"))
+number = int(input("enter a number between 0-10: "))
 
 tether = list(lists[number])
+print(tether)
 
 turns = 12
+correct = []
 
 while turns > 0:
     turns -= 1
-    my_letter = input("enter a letter")
-    if my_letter == tether:
-        print(my_letter)
-    else: 
-        print("-")
-
-#FIGURRE THIS OUT SOON
+    myletter = input("enter a letter")
+    for x in tether:
+        for y in my_letter:
+            if x == y:
+                correct.append(y)
+            else:
+                print("-")
+print(correct)
