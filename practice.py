70cb33caed24ac1dfa8d4409cdc672716b73623b
ariaspolett <ariaspolett21@gmail.com>
2024-09-19 13:59:27 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,33 +1,11 @@
-month = input("enter a month 1-12: ")
-date = input("enter a date 1-31: ")
-month = int(month)
-date = int(date)
+got_permission = [str(n) for n in input("Input a list of names separated by spaces").split()]
 
-def next_day(month, date):
-    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
-        if date < 31:
-            next_date = date + 1
-            next_month = month
-        else: 
-            next_month = month + 1 if month < 12 else 1
-            next_date = 1
-    elif month == 2:
-        if date < 28:
-            next_date = date + 1
-            next_month = month
-        else: 
-            next_month = month + 1
-            next_date = 1
-    elif month == 4 or month == 6 or month == 9 or month == 11:
-        if date < 30:
-            next_date = date + 1
-            next_month = month
-        else:
-            next_month = month + 1
-            next_date = 1
-    return next_month, next_date
+permission = dict.fromkeys(got_permission, "yes")
 
-result = next_day(month, date)
-if result:
-    next_month, next_date = result
-    print(next_month,next_date)
+# if "George" in permission_dictionary:
+# 	permission_dictionary["George"] = "no"
+	
+# if "Veronica" in permission_dictionary:
+# 	permission_dictionary.pop("Veronica")
+
+print(permission)
