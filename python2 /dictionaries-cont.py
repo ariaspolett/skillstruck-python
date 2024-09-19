1e0cd82154393cff8cd741fb7315eaf2b9440bba
ariaspolett <ariaspolett21@gmail.com>
2024-09-19 13:59:36 -0700

    exported from Skill Struck

--- python2 /dictionaries-cont.py
+++ python2 /dictionaries-cont.py
@@ -54,4 +54,14 @@ else:
     " "
 
 #Challenge - Permission Slips
-#This challenge was not completed.
+got_permission = [str(n) for n in input("Input a list of names separated by spaces").split()]
+
+permission = dict.fromkeys(got_permission, "yes")
+
+# if "George" in permission_dictionary:
+# 	permission_dictionary["George"] = "no"
+	
+# if "Veronica" in permission_dictionary:
+# 	permission_dictionary.pop("Veronica")
+
+print(permission)
