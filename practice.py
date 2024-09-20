9081b34e60190423c04ac24df9827bae3c6c0c24
ariaspolett <ariaspolett21@gmail.com>
2024-09-20 12:47:47 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,11 +1,1 @@
-got_permission = [str(n) for n in input("Input a list of names separated by spaces").split()]
-
-permission = dict.fromkeys(got_permission, "yes")
-
-# if "George" in permission_dictionary:
-# 	permission_dictionary["George"] = "no"
-	
-# if "Veronica" in permission_dictionary:
-# 	permission_dictionary.pop("Veronica")
-
-print(permission)
+nexxt
