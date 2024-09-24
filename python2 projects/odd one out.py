214d08d7bd6be4f8974bf57bffbc3fbb7a086689
ariaspolett <ariaspolett21@gmail.com>
2024-09-24 14:19:16 -0700

    exported from Skill Struck

--- python2 projects/odd one out.py
+++ python2 projects/odd one out.py
@@ -1,9 +1,20 @@
-words = input().split(" ")
+#CODE IS STILL NOT CORRECT 
+words = input().split()
 letter_count = []
-for x in words:
-	if x[0] not in letter_count:
-		letter_count.append(x[0])
+for word in words:
+	if word[0] not in letter_count:
+		letter_count.append(word[0])
 		letter_count.append(1)
 	else:
-		letter_count[letter_count.index(x[0])+1] += 1
-print(letter_count)
+		letter_count[letter_count.index(word[0])+1] += 1
+
+max_count = 0 
+most_common_letter = ""
+for i in range(0, len(letter_count), 2):
+    if letter_count[i+1] > max_count:
+        max_count = letter_count[i + 1]
+        most_common_letter = letter_count[i]
+
+common_words = [word for word in words if word.startswith(most_common_letter)]
+
+print(common_words)
