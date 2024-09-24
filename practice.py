1dc2c22ce143f6f4c0c75d2321a81ecf2a6864d1
ariaspolett <ariaspolett21@gmail.com>
2024-09-24 12:51:30 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,13 +1,1 @@
-flowers = [int(n) for n in input("How many blossoms are on each tree? ").split()]
-
-def orchard(flowers):
-    if len(flowers) == 1:
-        multiply = flowers[0] * 3
-        print(multiply)
-    else:
-        mid = len(flowers)//2
-        first = flowers[:mid]
-        second = flowers[mid:]
-        orchard(first)
-        orchard(second)
-orchard(flowers)
+nexxt
