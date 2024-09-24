8a1e88b2b8ae814d2a6c09c8d3df24a35976954b
ariaspolett <ariaspolett21@gmail.com>
2024-09-24 13:57:26 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,1 +1,10 @@
-nexxt
+final = []
+def numbers(num):
+    if num <= 1:
+        return num
+    else:
+        return(numbers(num-1) + numbers(num-2))
+length = int(input("enter a number"))
+for i in range(length):
+    final.append(numbers(i))
+print(final)
